cb98f5a3a0b2dd6ad15fb1caba92f7aacf0a2e3f
Darek Lyssy <darek.lyss@gmail.com>
2018-09-05 20:47:29 +0200

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get('/todos/:id', function(req, res) {
     }
     Todo.findById(id).then(function(todo){
         if (!todo){
-            return res.send(404).send()
+            return res.status(404).send()
         }
         res.send({todo});  
     }).catch(function(e) {
