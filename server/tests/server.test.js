67084653bea484426b0f33230fb75a29d498f839
Darek Lyssy <darek.lyss@gmail.com>
2018-09-10 15:37:57 +0200

    Add test for PATCH/tosos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,12 +5,15 @@ const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo')
 
-const todos = [{
+const todos = [{ 
     _id: new ObjectID(),
-    text: 'First test todo'
+    text: 'First test todo',
+    completed: false,
 },{
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 111
 }];
 
 beforeEach((done) => {
@@ -73,8 +76,9 @@ describe('GET /todos', function() {
 
 describe('GET /todos/:id', () => {
     it('Should return todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
         request(app)
-            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .get(`/todos/${hexId}`)
             .expect(200)
             .expect(function(res) {
                 expect(res.body.todo.text).toBe(todos[0].text)
@@ -130,4 +134,45 @@ describe('DELETE /todos/:id', () => {
             .expect(404)
             .end(done);
     });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('Should update the todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
+        let text = 'Text after change'
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({text, completed: true})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(typeof res.body.todo.completedAt).toBe('number');
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+                Todo.find({"completed": true}).then((todo) => {
+                    expect(todo.length).toBe(2);
+                    done();
+                }).catch((e) => done(e));
+            })
+    });    
+
+    it('Should clear completeAd when todo is not completed', (done) => {
+        let hexId = todos[1]._id.toHexString();
+        let text = 'Second text after change'
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({text, completed: false})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBe(null);
+            })
+            .end(done);
+    });
+
 });
