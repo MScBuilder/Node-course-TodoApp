0dcdb32572b94834551d6eb82e46923251250c45
Darek Lyssy <darek.lyss@gmail.com>
2018-09-12 11:01:19 +0200

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,57 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
         trim: true,
+        required: [true, 'User email address required'],
+        unique: true,
         validate: {
-            validator: function(v) {
-              return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
-            },
+            validator: validator.isEmail,
             message: props => `${props.value} is not a valid email address!`
-          },
-          required: [true, 'User email address required']
-    }
+        }
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: 6
+    },
+
+    tokens: [{
+        access: {
+            type: String,
+            required: [true, 'Access property is required']
+        },
+        token: {
+            type: String,
+            required: [true, 'Token is required']
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+const User = mongoose.model('User', UserSchema );
 
 module.exports = {User};
